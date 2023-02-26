532b59439f535561d75149cda2e9c3b49e9f0f61
Rebecca-Collins <rebeccacollins@shaw.ca>
2023-02-25 16:08:10 -0800

    added highlighting colour to my body text

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -3,15 +3,10 @@ import { Link } from "react-router-dom";
 import Typewriter from "../../components/Typewriter/Typewriter";
 import Skills from "../../components/Skills/Skills";
 
-
 function HomePage() {
- 
-
-  
-  
   return (
-      <main className="home">
-       <div className="home__container"> 
+    <main className="home">
+      <div className="home__container">
         <div className="home__heading-container">
           <div className="home__text-typewriter">
             <Typewriter text="Rebecca Collins." />
@@ -21,25 +16,40 @@ function HomePage() {
         <div className=" home__memoji"></div>
         <div className="home__subheading-container">
           <p className="home__paragraph">
-            As someone who is deeply passionate about the industry, I constantly
-            stay up-to-date with the latest trends and cutting-edge
-            technologies. My goal is always to create exceptional results,
+            As someone who is{" "}
+            <span className="home__highlight-text">deeply passionate</span>{" "}
+            about the industry, I constantly stay up-to-date with the latest
+            trends and{" "}
+            <span className="home__highlight-text">
+              cutting-edge technologies
+            </span>
+            . My goal is always to create{" "}
+            <span className="home__highlight-text">exceptional results</span>,
             whether I'm designing a modern website or implementing innovative
-            marketing strategies. I believe that my dedication to staying ahead
-            of the curve and my commitment to collaboration make me a valuable
-            asset to any team. Together, we can give your brand the competitive
-            edge it deserves.
+            marketing strategies. I believe that my{" "}
+            <span className="home__highlight-text">dedication</span> to staying
+            ahead of the curve and my commitment to{" "}
+            <span className="home__highlight-text">collaboration</span> make me
+            a <span className="home__highlight-text">valuable asset</span> to
+            any team. Together, we can give your brand the{" "}
+            <span className="home__highlight-text">competitive edge</span> it
+            deserves.
           </p>
           <p className="home__paragraph">
-            Beyond my work, I'm dedicated to personal and professional growth.
-            When I'm not tackling the latest project, you'll find me playing
+            Beyond my work, I'm dedicated to{" "}
+            <span className="home__highlight-text">
+              personal and professional growth
+            </span>
+            . When I'm not tackling the latest project, you'll find me playing
             soccer, volunteering, or staying up-to-date with the latest
-            technology advancements. I'm passionate about problem-solving and
-            driven to make a positive impact, and I'm looking for a company that
-            shares those same values.
+            technology advancements. I am{" "}
+            <span className="home__highlight-text">dedicated</span> to
+            problem-solving and driven to make a{" "}
+            <span className="home__highlight-text">positive impact</span>, and
+            I'm looking for a company that shares those same values.
           </p>
         </div>
-        <Skills/>
+        <Skills />
         <div className="home__link-container">
           <Link className="home__link" to="/contact">
             Get In Touch
@@ -54,43 +64,3 @@ function HomePage() {
 }
 
 export default HomePage;
-
-
-  /* <p className="home__paragraph">
-            I stay current with industry trends and use cutting-edge
-            technologies to bring your brand to life. Dedicated to exceptional
-            results, whether creating a modern website or complex software.
-            Let's shape the future and give your brand a competitive edge.
-          </p> */
-
-
-  /* <p className="home__paragraph">
-            I am dedicated to delivering high-quality work and making a positive
-            impact through my skills and expertise. In my free time, I enjoy
-            playing soccer, volunteering and keeping up with the latest
-            technology advancements. I am looking for a company that shares my
-            passion for innovation and problem-solving.
-          </p> */
-
-
-// {/* <p className="home__paragraph">
-//             My approach is centred around staying ahead of industry trends and
-//             utilizing cutting-edge technologies to bring your company's values
-//             to life for your clients. I am dedicated to delivering exceptional
-//             results, whether it's through the creation of a sleek and modern
-//             website or the development of a complex software system. Let's work
-//             together to shape the future of technology and give your brand a
-//             competitive edge. My portfolio showcases the diverse range of
-//             projects I've tackled and demonstrates the skills I bring to the
-//             table.
-//           </p> */}
-
-//             {/* <p className="home__paragraph">
-//             If you're looking for a web developer who is dedicated to delivering
-//             high-quality results and is always eager to learn and grow, look no
-//             further. My approach, as outlined in the previous paragraph, is
-//             centered around staying ahead of industry trends and utilizing
-//             cutting-edge technologies to bring your company's values to life for
-//             your clients. Let's work together to bring your vision to life and
-//             give your brand a competitive edge.
-//           </p> */}
